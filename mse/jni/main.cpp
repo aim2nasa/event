ebaf49001e4c1c1640f47084a941caf421a3ba85
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-10 03:33:20 +0000

    define scan_dir and apply it

--- mse/jni/main.cpp
+++ mse/jni/main.cpp
@@ -1,11 +1,59 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <dirent.h>
+
+static int scan_dir(const char *dirname)
+{
+    char devname[PATH_MAX];
+    char *filename;
+    DIR *dir;
+    struct dirent *de;
+
+    printf("scan_dir start %s\n",dirname);
+
+    //printf("dir(%s) opening...\n",dirname);
+    dir = opendir(dirname);
+    if(dir == NULL)
+        return -1;
+    printf("dir(%s) opened\n",dirname);
+
+    strcpy(devname, dirname);
+    filename = devname + strlen(devname);
+    *filename++ = '/';
+
+    //printf("filename=%s\n",filename);
+    while((de = readdir(dir))) {
+        //printf("readdir, (%s)\n",de->d_name);
+        if(de->d_name[0] == '.' &&
+           (de->d_name[1] == '\0' ||
+            (de->d_name[1] == '.' && de->d_name[2] == '\0'))) {
+	    //printf("continue,d_name:%d,%d,%d\n",de->d_name[0],de->d_name[1],de->d_name[1],de->d_name[2]);
+            continue;
+	}
+
+        strcpy(filename, de->d_name);
+        printf("device(%s) opening...\n",devname);
+        //open_device(devname);
+    }
+    closedir(dir);
+    printf("dir(%s) closed\n",dirname);
+
+    printf("scan_dir end %s\n",dirname);
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    if(argc<2) {
-        fprintf(stderr, "usage: %s [dump file name]\n", argv[0]);
+    const char *path = ".";
+    if(argc<3) {
+        fprintf(stderr, "usage: %s [path of files] [file prefix]\n", argv[0]);
+        return 1;
+    }
+    
+    path = argv[1];
+    if(scan_dir(path) < 0) {
+        fprintf(stderr, "scan dir failed for %s\n", path);
         return 1;
     }
 
