6db2f43d4da65385e1bd3f9085787bfb1cf05d1c
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-11 07:46:27 +0000

    read file open

--- ep/jni/main.cpp
+++ ep/jni/main.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
     printf("event play x1 version\n");
+    if(argc<2) {
+        printf("usage:ep <event dump file>\n");
+        return -1;
+    }
+
+    int fd;
+    if((fd=open(argv[1],O_RDONLY)) < 0) {
+        fprintf(stderr, "could not open %s, %s\n",argv[1],strerror(errno));
+        return -1;
+    }
+
+    close(fd);
     printf("end\n");
     return 0;
 }
