26fbefbb8c42831926df272492199dddae27e8e6
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-11 08:16:58 +0000

    file size and events count

--- branches/x1/ep/jni/main.cpp
+++ branches/x1/ep/jni/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <linux/input.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +11,15 @@ int main(int argc, char *argv[])
         printf("usage:ep <event dump file>\n");
         return -1;
     }
+
+    struct stat fileInfo;
+    if(stat(argv[1],&fileInfo)==-1) {
+        fprintf(stderr, "stat error %s, %s\n",argv[1],strerror(errno));
+        return -1;
+    }
+    long long eventCount = fileInfo.st_size
+        /(long long)(sizeof(struct input_event)+sizeof(int));
+    printf("file size:%lldbytes,events:%lld\n",fileInfo.st_size,eventCount);  
 
     int fd;
     if((fd=open(argv[1],O_RDONLY)) < 0) {
