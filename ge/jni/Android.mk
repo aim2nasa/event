1ec94c3ac903f156e1e2d58516a20d63cf6b1130
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-12 05:27:44 +0000

    define event recording library, liber and let it use this lib

--- ge/jni/Android.mk
+++ ge/jni/Android.mk
@@ -1,13 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
+#Library
+include $(CLEAR_VARS)
+LOCAL_MODULE := liber
+LOCAL_SRC_FILES := CEvtRec.cpp 
+include $(BUILD_SHARED_LIBRARY)
+
 #Executable
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ge
-LOCAL_SRC_FILES := main.cpp \
-	           CEvtRec.cpp
+LOCAL_SRC_FILES := main.cpp
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_C_INCLUDES += /home/skwak/android-ndk-r8e/platforms/android-14/arch-arm/usr/include \
                     /home/skwak/x1/ge/jni
+LOCAL_SHARED_LIBRARIES := liber
 
 include $(BUILD_EXECUTABLE)
--- ge/upExec.sh
+++ ge/upExec.sh
@@ -1,1 +1,2 @@
 adb push libs/armeabi/ge /data/local/tmp
+adb push libs/armeabi/liber.so /data/local/tmp
