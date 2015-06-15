cba2aa3e413aa7beaca3fd672c71cdfad8150dce
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-15 12:03:54 +0000

    crete folder for common things and let use this

---
+++ branches/x1/common/def.h
@@ -0,0 +1,8 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define EVENT_RECORD_START      (0xffabc001)
+#define EVENT_RECORD_STOP       (0xffabc002)
+#define TERMINATE_SERVER        (0xffabcfff)
+
+#endif
