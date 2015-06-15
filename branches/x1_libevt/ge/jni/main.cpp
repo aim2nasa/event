09e8de2e7a3d158dd457b388e9caf3f6f71938b6
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-15 07:23:37 +0000

    libevt를 작성하여 ep와 ge가 이를 사용하여 동작하도록 만듬

---
+++ branches/x1_libevt/ge/jni/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "CEvtRec.h"
+#include "CDump.h"
+
+void sig_handler(int signo)
+{
+    printf("\nsignal(%d) received\n",signo);
+    CEvtRec::instance()->stop();
+}
+
+int main(int argc, char *argv[])
+{
+    printf("get event x1 version\n");
+    if(argc<3) {
+        printf("usage:ge <dump file name> <event#> <event#>... <event#>\n");
+        return -1;
+    }
+   
+    CDump d;
+    if(d.dumpOpen(argv[1]))
+        printf("dump file:%s opened\n",argv[1]);
+    else{
+        printf("dump file:%s open failure\n",argv[1]);
+        return -1;
+    }   
+        
+    CEvtRec& er=*CEvtRec::instance();
+    er.evtDump(&d);
+    for(int i=3;i<=argc;i++) er.addDevice(atoi(argv[i-1]));
+
+    printf("event# :");
+    for(std::list<int>::iterator it=er.devList().begin();it!=er.devList().end();it++) 
+        printf(" %d ",*it);
+    printf("\n%d event# read in\n",er.devices()); 
+
+    if(er.devOpen()) {
+        printf("%d devices opened\n",er.devices());
+    }else{
+        printf("error in opening event#%d\n",er.errDev());
+        return -1;
+    }
+ 
+    struct sigaction act;
+    memset(&act,0,sizeof(act));
+    act.sa_handler = sig_handler; 
+    sigaction(SIGINT,&act,0);
+
+    printf("reading for %d devices...\n",er.devices());
+    er.start();
+    er.wait();
+    printf("reading done\n");
+
+    d.dumpClose();
+    printf("end\n");
+    return 0;
+}
---
+++ branches/x1_libevt/ge/upExec.sh
@@ -0,0 +1,2 @@
+adb push libs/armeabi/ge /data/local/tmp
+adb push libs/armeabi/libevt.so /data/local/tmp
