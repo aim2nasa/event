395968338825637de3e52dd02418aaf12f022dec
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-15 07:04:23 +0000

    move CEvtPlay to lib and change accordingly

--- ep/jni/Android.mk
+++ ep/jni/Android.mk
@@ -1,11 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+#Library
+include $(CLEAR_VARS)
+LOCAL_MODULE := libevt
+LOCAL_SRC_FILES := /home/skwak/x1/lib/libs/armeabi/libevt.so
+include $(PREBUILT_SHARED_LIBRARY)
+
 #Executable
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ep
-LOCAL_SRC_FILES := main.cpp \
-                   CEvtPlay.cpp
+LOCAL_SRC_FILES := main.cpp
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+LOCAL_C_INCLUDES += /home/skwak/x1/lib/jni
+LOCAL_SHARED_LIBRARIES := libevt
 
 include $(BUILD_EXECUTABLE)
--- ep/run.sh
+++ ep/run.sh
@@ -1,1 +1,1 @@
-adb shell "/data/local/tmp/ep"
+adb shell "LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/ep"
