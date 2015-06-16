15a3457ec4d3455eca06b8aff3ba993ab24f4c8d
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-16 06:30:13 +0000

    define INIT code

--- common/def.h
+++ common/def.h
@@ -1,6 +1,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#define EVENT_RECORD_INIT       (0xffabc000)
 #define EVENT_RECORD_START      (0xffabc001)
 #define EVENT_RECORD_DATA       (0xffabc002)
 #define EVENT_RECORD_STOP       (0xffabc003)
