8162a6d5c826233ae6aae97b1bad058a346cefdc
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-16 03:07:00 +0000

    add log mask and trace for main

--- eSvr/jni/server.cpp
+++ eSvr/jni/server.cpp
@@ -9,6 +9,9 @@
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
+    ACE_TRACE(ACE_TEXT("main"));
+    ACE_LOG_MSG->priority_mask( LM_INFO|LM_ERROR, ACE_Log_Msg::PROCESS);
+
     u_short server_port = argc > 1 ? ACE_OS::atoi(argv[1]) : SERVER_PORT;
     ACE_DEBUG((LM_INFO, "(%t) server start at port:%d\n", server_port));
 
