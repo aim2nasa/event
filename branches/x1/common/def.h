bf34eb0cff4bebad8c7f7419f37e78e686f38b0f
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-16 01:45:55 +0000

    define mode commands and let CEvtRcv work with command code

--- branches/x1/common/def.h
+++ branches/x1/common/def.h
@@ -2,7 +2,9 @@
 #define __COMMON_H__
 
 #define EVENT_RECORD_START      (0xffabc001)
-#define EVENT_RECORD_STOP       (0xffabc002)
+#define EVENT_RECORD_DATA       (0xffabc002)
+#define EVENT_RECORD_STOP       (0xffabc003)
+#define TERMINATE_CLIENT        (0xffabcffe)
 #define TERMINATE_SERVER        (0xffabcfff)
 
 #endif
--- branches/x1/eRcv/jni/CEvtRcv.cpp
+++ branches/x1/eRcv/jni/CEvtRcv.cpp
@@ -1,5 +1,6 @@
 #include "CEvtRcv.h"
 #include "ace/SOCK_Stream.h"
+#include "../../common/def.h"
 
 CEvtRcv::CEvtRcv(ACE_SOCK_Stream* p)
 :_pStream(p)
@@ -14,13 +15,34 @@ CEvtRcv::~CEvtRcv()
 
 int CEvtRcv::svc()
 {
+    ACE_TRACE("CEvtRcv::svc");
+
+    int msg;
     char buf[BUFSIZ];
     while (1){
-        ssize_t rcvSize = _pStream->recv(buf,sizeof(buf));
+        ssize_t rcvSize = _pStream->recv_n(&msg,sizeof(int));
 	if (rcvSize <= 0) {
-	    ACE_DEBUG((LM_ERROR, ACE_TEXT("(%P|%t) Connection closed while receiving header\n")));
+	    ACE_DEBUG((LM_ERROR, ACE_TEXT("(%P|%t) error in receiving msg\n")));
 	    return -1;
 	}
+
+        switch(msg){
+        case EVENT_RECORD_START:
+	    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Event Record Start(0x%x)\n"),msg));
+            break;
+        case EVENT_RECORD_DATA:
+	    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Event Record Data(0x%x)\n"),msg));
+            break;
+        case EVENT_RECORD_STOP:
+	    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Event Record Stop(0x%x)\n"),msg));
+            break;
+        case TERMINATE_CLIENT:
+	    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Terminate(0x%x)\n"),msg));
+            return 0;
+        default:
+	    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Undefined command(0x%x)\n"),msg));
+            break;
+        }
     }
     return 0;
 }
