dfd4a96a0a2bf401b2fe51813b134d1ec3bdd7b7
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-18 11:13:37 +0000

    이벤트를 읽어 각 장치가 touch인지 Key장치 인지 구분하여 map자료구조에 넣는다

--- eRcv/eClass/CClassifier.cpp
+++ eRcv/eClass/CClassifier.cpp
@@ -14,5 +14,14 @@ CClassifier::~CClassifier()
 int CClassifier::addEvt(int device, long sec, long usec, int type, int code, int value)
 {
 	ACE_DEBUG((LM_DEBUG, "[%T] dev:%02d,time:%d.%06d,type:%04x,code:%04x,val:%08x\n",device,sec,usec,type,code,value));
+
+	std::pair<std::map<int,DEV_TYPE>::iterator, bool> ret;
+	if (EV_KEY == (DEV_TYPE)type) {
+		ret = _devMap.insert(std::make_pair(device, KEY));
+		if(ret.second) ACE_DEBUG((LM_DEBUG, "[%T] dev:%02d type:%04x KEY TYPE registered(map size:%d)\n",device, type,_devMap.size()));
+	}else if (EV_ABS == (DEV_TYPE)type){
+		ret = _devMap.insert(std::make_pair(device, TOUCH));
+		if (ret.second) ACE_DEBUG((LM_DEBUG, "[%T] dev:%02d type:%04x TOUCH TYPE registered(map size:%d)\n",device, type, _devMap.size()));
+	}
 	return 0;
 }
--- eRcv/eClass/CClassifier.h
+++ eRcv/eClass/CClassifier.h
@@ -1,12 +1,31 @@
 #ifndef __CCLASSIFIER_H__
 #define __CCLASSIFIER_H__
 
+#include <map>
+
+//EVENT TYPES
+#define EV_SYN		0x00
+#define EV_KEY		0x01
+#define EV_ABS		0x03
+
+//Synchronization events
+#define SYN_REPORT	0
+
+//Absolute
+#define ABS_MT_TRACKING_ID      0x39    /* Unique ID of initiated contact */
+
 class CClassifier {
 public:
+	enum DEV_TYPE{ TOUCH, KEY };
+	enum TOUCH_TYPE{ TAP, SWIPE, MULTITOUCH };
+
 	CClassifier();
 	virtual ~CClassifier();
 
 	int addEvt(int device, long sec, long usec, int type, int code, int value);
+
+protected:
+	std::map<int, DEV_TYPE> _devMap;
 };
 
 #endif
