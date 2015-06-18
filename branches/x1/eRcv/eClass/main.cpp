01fca8daf238f2d331427273c3d6863063b5bbb6
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-18 08:09:53 +0000

    인자 읽어들이도록 구현

--- branches/x1/eRcv/eClass/main.cpp
+++ branches/x1/eRcv/eClass/main.cpp
@@ -1,9 +1,18 @@
 #include "ace/Log_Msg.h" 
+#include "ace/OS.h"
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
 	ACE_TRACE(ACE_TEXT("main"));
 	ACE_LOG_MSG->priority_mask(LM_INFO|LM_ERROR|LM_DEBUG, ACE_Log_Msg::PROCESS);
+
+	if (argc < 2) {
+		ACE_DEBUG((LM_INFO, "usage:eClass <event dump file>\n"));
+		ACE_RETURN(-1);
+	}
+
+	FILE* fp = ACE_OS::fopen(argv[1], ACE_TEXT("rb"));
+	ACE_OS::fclose(fp);
 
 	ACE_DEBUG((LM_INFO, "[%T] end\n"));
 	ACE_RETURN(0);
