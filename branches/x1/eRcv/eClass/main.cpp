1df4311f8d04e18fa4fd4b9c38027a9b9ebf93f2
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-18 09:09:48 +0000

    파일 읽어들이는 구현

--- branches/x1/eRcv/eClass/main.cpp
+++ branches/x1/eRcv/eClass/main.cpp
@@ -1,5 +1,18 @@
 #include "ace/Log_Msg.h" 
 #include "ace/OS.h"
+#include "ace/SString.h"
+
+typedef unsigned short _u16;
+typedef unsigned int _u32;
+typedef short _s16;
+typedef int _s32;
+
+struct input_event {
+	struct timeval time;
+	_u16 type;
+	_u16 code;
+	_s32 value;
+};
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
@@ -10,9 +23,20 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 		ACE_DEBUG((LM_INFO, "usage:eClass <event dump file>\n"));
 		ACE_RETURN(-1);
 	}
+	ACE_TString filename(argv[1]);
+	ACE_DEBUG((LM_INFO, "[%T] event dump file:%s\n", filename.c_str()));
+	ACE_DEBUG((LM_DEBUG, "[%T] sizeof(timeval)=%dbytes, sizeof(event)=%dbytes\n", sizeof(timeval),sizeof(struct input_event)));
 
-	FILE* fp = ACE_OS::fopen(argv[1], ACE_TEXT("rb"));
+	char buffer[BUFSIZ];
+	size_t size, readUnit = sizeof(struct input_event) + sizeof(int),totalRead=0;
+	FILE* fp = ACE_OS::fopen(filename.c_str(), ACE_TEXT("rb"));
+	while (1){
+		size = ACE_OS::fread(buffer, 1, readUnit, fp);
+		if (size != readUnit) break;
+		totalRead += size;
+	}
 	ACE_OS::fclose(fp);
+	ACE_DEBUG((LM_INFO, "[%T] %dbytes read from %s\n",totalRead,filename.c_str()));
 
 	ACE_DEBUG((LM_INFO, "[%T] end\n"));
 	ACE_RETURN(0);
