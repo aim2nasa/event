1c024ebd25aa9b748e72125b1ef9912f51546825
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-18 01:54:38 +0000

    ge에서 실행파일및 폴더 이름을 er로 변경함 (event recorder)

--- branches/x1/er/jni/Android.mk
+++ branches/x1/er/jni/Android.mk
@@ -9,12 +9,12 @@ include $(PREBUILT_SHARED_LIBRARY)
 #Executable
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := ge
+LOCAL_MODULE := er
 LOCAL_SRC_FILES := main.cpp \
                    CDump.cpp
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_C_INCLUDES += /home/skwak/android-ndk-r8e/platforms/android-14/arch-arm/usr/include \
-                    /home/skwak/x1/ge/jni \
+                    /home/skwak/x1/er/jni \
                     /home/skwak/x1/lib/jni
 LOCAL_SHARED_LIBRARIES := libevt
 
