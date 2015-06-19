74ea93a799050b7c339251f455a7618257f3b5c9
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-19 04:49:57 +0000

    index로 변수 이름 변경

--- branches/x1/eRcv/eClass/CClassifier.cpp
+++ branches/x1/eRcv/eClass/CClassifier.cpp
@@ -18,44 +18,44 @@ void CClassifier::reset()
 	_prevDev = -1;
 }
 
-int CClassifier::addEvt(long evtOrder,int device, long sec, long usec, int type, int code, int value)
+int CClassifier::addEvt(long index, int device, long sec, long usec, int type, int code, int value)
 {
-	ACE_DEBUG((LM_DEBUG, "[%T] order:%d,dev:%02d,time:%d.%06d,type:%04x,code:%04x,val:%08x\n",evtOrder,device,sec,usec,type,code,value));
+	ACE_DEBUG((LM_DEBUG, "[%T] index:%d,dev:%02d,time:%d.%06d,type:%04x,code:%04x,val:%08x\n",index,device,sec,usec,type,code,value));
 
 	std::pair<std::map<int,DEV_TYPE>::iterator, bool> ret;
 	if (EV_KEY == (DEV_TYPE)type) {
 		if (_devMap.insert(std::make_pair(device, KEY)).second)
-			onNewKeyDevice(evtOrder,device,sec,usec,type,code,value);
+			onNewKeyDevice(index,device,sec,usec,type,code,value);
 		else
-			onExistingKeyDevice(evtOrder, device, sec, usec, type, code, value);
+			onExistingKeyDevice(index, device, sec, usec, type, code, value);
 			
 	}else if (EV_ABS == (DEV_TYPE)type){
 		if (_devMap.insert(std::make_pair(device, TOUCH)).second)
-			onNewTouchDevice(evtOrder, device, sec, usec, type, code, value);
+			onNewTouchDevice(index, device, sec, usec, type, code, value);
 		else
-			onExistingTouchDevice(evtOrder, device, sec, usec, type, code, value);
+			onExistingTouchDevice(index, device, sec, usec, type, code, value);
 	}
 
 	_prevDev = device;
 	return 0;
 }
 
-void CClassifier::onNewKeyDevice(long evtOrder, int device, long sec, long usec, int type, int code, int value)
+void CClassifier::onNewKeyDevice(long index, int device, long sec, long usec, int type, int code, int value)
 {
-	ACE_DEBUG((LM_DEBUG, "[%T] order:%d,dev:%02d type:%04x KEY TYPE registered(map size:%d)\n", evtOrder, device, type, _devMap.size()));
+	ACE_DEBUG((LM_DEBUG, "[%T] index:%d,dev:%02d type:%04x KEY TYPE registered(map size:%d)\n", index, device, type, _devMap.size()));
 }
 
-void CClassifier::onExistingKeyDevice(long evtOrder, int device, long sec, long usec, int type, int code, int value)
+void CClassifier::onExistingKeyDevice(long index, int device, long sec, long usec, int type, int code, int value)
 {
 
 }
 
-void CClassifier::onNewTouchDevice(long evtOrder, int device, long sec, long usec, int type, int code, int value)
+void CClassifier::onNewTouchDevice(long index, int device, long sec, long usec, int type, int code, int value)
 {
-	ACE_DEBUG((LM_DEBUG, "[%T] order:%d,dev:%02d type:%04x TOUCH TYPE registered(map size:%d)\n", evtOrder, device, type, _devMap.size()));
+	ACE_DEBUG((LM_DEBUG, "[%T] index:%d,dev:%02d type:%04x TOUCH TYPE registered(map size:%d)\n", index, device, type, _devMap.size()));
 }
 
-void CClassifier::onExistingTouchDevice(long evtOrder, int device, long sec, long usec, int type, int code, int value)
+void CClassifier::onExistingTouchDevice(long index, int device, long sec, long usec, int type, int code, int value)
 {
 
 }
