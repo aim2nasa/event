6bbad22d9bc5f38c1f836d5a6c6d349d1b5a2dd5
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-19 04:43:51 +0000

    addEvt의 인자 evtOrder : 파일에서 이벤트 레코드가 0번째 인덱스로 부터 시작해서 몇번째 인덱스인지 알려주는 인자 추가 디바이스가 새로 등록되거나 중복된 것일때 훅 메소드 정의

--- branches/x1/eRcv/eClass/main.cpp
+++ branches/x1/eRcv/eClass/main.cpp
@@ -22,14 +22,16 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 	CRecord rec;
 	size_t size,totalRead=0;
 	FILE* fp = ACE_OS::fopen(filename.c_str(), ACE_TEXT("rb"));
+	long evtOrder = 0;
 	while (1){
 		size = ACE_OS::fread(&rec, 1, sizeof(rec), fp);
 		if (size != sizeof(rec)) break;
 		totalRead += size;
-		if (cf.addEvt(rec._device, rec._event.time.tv_sec, rec._event.time.tv_usec, rec._event.type, rec._event.code, rec._event.value) < 0) {
+		if (cf.addEvt(evtOrder,rec._device, rec._event.time.tv_sec, rec._event.time.tv_usec, rec._event.type, rec._event.code, rec._event.value) < 0) {
 			ACE_DEBUG((LM_ERROR, "[%T] error in add event\n"));
 			break;
 		}
+		evtOrder++;
 	}
 	ACE_OS::fclose(fp);
 	ACE_DEBUG((LM_INFO, "[%T] %dbytes read from %s\n",totalRead,filename.c_str()));
