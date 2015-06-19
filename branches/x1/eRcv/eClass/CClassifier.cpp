d966dc9990452aae8ca4e339f0f7b87a0c215806
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-19 01:52:55 +0000

    reset 메소드 추가

--- branches/x1/eRcv/eClass/CClassifier.cpp
+++ branches/x1/eRcv/eClass/CClassifier.cpp
@@ -11,6 +11,11 @@ CClassifier::~CClassifier()
 
 }
 
+void CClassifier::reset()
+{
+	_devMap.clear();
+}
+
 int CClassifier::addEvt(int device, long sec, long usec, int type, int code, int value)
 {
 	ACE_DEBUG((LM_DEBUG, "[%T] dev:%02d,time:%d.%06d,type:%04x,code:%04x,val:%08x\n",device,sec,usec,type,code,value));
