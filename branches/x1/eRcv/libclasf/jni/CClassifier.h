5b7d0e2b48e35ddda40f8d0ddabc2ae56d3f5dce
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-23 09:07:48 +0000

    IClassifyNoti를 별도의 헤더파일로 분리

--- branches/x1/eRcv/libclasf/jni/CClassifier.h
+++ branches/x1/eRcv/libclasf/jni/CClassifier.h
@@ -3,23 +3,10 @@
 
 #include "macro.h"
 #include <map>
+#include "IClassifyNoti.h"
 
 class CMtTracking;
 class CKeyTracking;
-
-class DLLEXPORT IClassifyNoti{
-public:
-	enum DEV_TYPE{ TOUCH, KEY };
-	enum TOUCH_TYPE{ TAP, SWIPE, MULTITOUCH };
-	enum ERR_CODE{ DEV_MIXED,KEY_SEQ };
-
-	virtual ~IClassifyNoti(){}
-
-	virtual void onNewDevice(int device,DEV_TYPE type,int index) = 0;
-	virtual void onKeyEvent(int device,int startIndex, int endIndex) = 0;
-	virtual void onTouchEvent(int device,TOUCH_TYPE type, int startIndex, int endIndex) = 0;
-	virtual void onError(ERR_CODE code) = 0;
-};
 
 class DLLEXPORT CClassifier {
 public:
