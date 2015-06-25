593d4794eee33a74604bf35f491b6297d328a414
skwak <skwak@218462c8-ce82-0147-8d19-363e55a3fe70>
2015-06-25 05:34:38 +0000

    파일 정보(이벤트 리스트 자료구조)를 내보내 주는 클래스 작성하여 라이브러리에 추가

---
+++ branches/x1/eRcv/jni/CFileInfo.cpp
@@ -0,0 +1,79 @@
+#include "CFileInfo.h"
+#include "CRecord.h"
+#include "CClassifier.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS.h"
+
+int CFileInfo::analyze(const char *file,CResult *result)
+{
+	FILE* fp = ACE_OS::fopen(file, ACE_TEXT("rb"));
+	if (!fp) {
+		ACE_DEBUG((LM_ERROR, "%s open error\n", file));
+		ACE_RETURN(-10);
+	}
+
+	CClassifier cf;
+	cf.notify(result);
+	CRecord rec;
+	size_t size, totalRead = 0;
+	long index = 0,err=0;
+	while (1){
+		size = ACE_OS::fread(&rec, 1, sizeof(rec), fp);
+		if (size != sizeof(rec)) break;
+		totalRead += size;
+		if (cf.addEvt(index, rec._device, rec._event.time.tv_sec, rec._event.time.tv_usec, rec._event.type, rec._event.code, rec._event.value) < 0) {
+			err++;
+			ACE_DEBUG((LM_ERROR, "[%T] error in add event\n"));
+			break;
+		}
+		index++;
+	}
+	ACE_OS::fclose(fp);
+	if (err == 0)
+		ACE_RETURN(0);
+	else
+		ACE_RETURN(-20);
+}
+
+CResult::CResult()
+:_pList(new UEVT_LIST())
+{
+
+}
+
+CResult::~CResult()
+{
+	delete _pList;
+}
+
+void CResult::reset()
+{
+	_pList->clear();
+}
+
+UEVT_LIST& CResult::list()
+{
+	return *_pList;
+}
+
+void CResult::onNewDevice(int device, DEV_TYPE type, int index)
+{
+
+}
+
+void CResult::onKeyEvent(int device, int startIndex, int endIndex)
+{
+	CUserEvent ue(KEY,device,startIndex,endIndex);
+	_pList->push_back(ue);
+}
+
+void CResult::onTouchEvent(int device, TOUCH_TYPE type, int startIndex, int endIndex)
+{
+	CUserEvent ue(TOUCH, device, startIndex, endIndex);
+	_pList->push_back(ue);
+}
+
+void CResult::onError(ERR_CODE code)
+{
+
+}
