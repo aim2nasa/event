2e140e0b627f78da860cbe92e3f86e9c7beb8511
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-07-29 03:03:44 +0000

    결함 #193 삼성 갤럭시 6 엣지에서 백 버튼이 먹지 않는다
    터치 장치에서 code값이 키값의 범위에 들경우에는 키가 눌린 처리를 해준다

--- eRcv/libclasf/jni/CKeyTracking.h
+++ eRcv/libclasf/jni/CKeyTracking.h
@@ -12,6 +12,15 @@ public:
 		_index = _device = _code = _value = -1;
 	}
 
+	void set(bool tracking, int index, int device, int code, int value)
+	{
+		_tracking = tracking;
+		_index = index;
+		_device = device;
+		_code = code;
+		_value = value;
+	}
+
 	int _index;
 	int _device;
 	int _code;
